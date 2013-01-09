3a7e95ad09eb271e07f2f12e393f7a5882cfcb0f
Julien Grillot <julien.grillot@gmail.com>
2013-01-09 01:00:46 +0100

    Pagedown for instant preview
    Also fix break-word in preview
    and in forums/topics/messages tables

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
 //= require jquery.caretposition
@@ -18,7 +6,9 @@
 //= require textarea.jquery
 //= require garlic
 //= require bootstrap
-//= require markdown-toolbar
+//= require markdown.converter
+//= require markdown.sanitizer
+//= require markdown.editor
 //= require gritter
 //= require users
 //= require forums
@@ -31,7 +21,18 @@
 var values = [];
 $(document).ready(function() {
   // Handle autocomplete and maxlength
-  $("textarea").sew({values: values}).textarea();
+  $('textarea').sew({values: values}).textarea();
+
+  $wmd = $('#wmd-input');
+  if ($wmd.length) {
+    var converter = Markdown.getSanitizingConverter();
+    var editor = new Markdown.Editor(converter);
+    $wmd.keyup(function() {
+      $('.preview').show('fast');
+    });
+    editor.run();
+    $('.wmd-button').tooltip();
+  }
 
   // Does not submit forms twice
   $('form:not([data-remote])').submit(function(){
