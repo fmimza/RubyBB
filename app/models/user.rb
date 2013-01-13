cc4eb8e5f8441780167b34b6e02dd6e9b073072b
Julien Grillot <julien.grillot@gmail.com>
2013-01-14 00:47:46 +0100

    User Devise Confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :lockable
+         :confirmable, :omniauthable, :lockable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
