b67f152ce66f10b565b9ae96c618d97994c6596b
Julien Grillot <julien.grillot@gmail.com>
2013-01-16 00:09:03 +0100

    Anonymous user website tour test

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,13 +3,38 @@ FactoryGirl.define do
     name { Faker::Name.name }
     email { Faker::Internet.email }
     password 'password'
-    sysadmin false
+
+    factory :admin do
+      sysadmin true
+    end
   end
 
-  factory :admin, class: User do
-    name { Faker::Name.name }
-    email { Faker::Internet.email }
-    password 'password'
-    sysadmin true
+  factory :forum do
+    name { Faker::Lorem.sentence(2) }
+  end
+
+  factory :topic do
+    user
+    forum
+    name { Faker::Lorem.sentence(3) }
+    after :create do |topic, evaluator|
+      FactoryGirl.create_list(:message, 1, topic: topic, user: evaluator.user, forum: evaluator.forum)
+    end
+  end
+
+  factory :message, aliases: [:first_message, :last_message] do
+    user
+    forum
+    topic
+    content { Faker::Lorem.paragraph }
+    rendered_content { "<p>#{content}</p>" }
+  end
+
+  factory :small_message do
+    user
+    forum
+    topic
+    message
+    content { Faker::Lorem.sentence(3) }
   end
 end
---
+++ spec/features/visitor_visits_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+feature 'Visitor visits' do
+  scenario 'forums index, topics and messages' do
+    forum = create :forum
+    topic = create :topic, forum: forum
+    message = topic.messages.first
+    small_message = create :small_message, forum: forum, topic: topic, message: message
+
+    visit root_path
+    expect(page).to have_content(forum.name)
+    page.find(:xpath, "//a[@href='#{forum_path(forum)}']").click
+    expect(page).to have_content(topic.name)
+    page.find(:xpath, "//a[@href='#{topic_path(topic)}']").click
+    expect(page).to have_content(message.content)
+    expect(page).to have_content(small_message.content)
+  end
+
+  scenario 'users index and a profile' do
+    user = create :user
+
+    visit users_path
+    expect(page).to have_content(user.name)
+    page.find(:xpath, "//a[@href='#{user_path(user)}']").click
+    expect(page).to have_content(user.name)
+  end
+end
