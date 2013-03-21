c58d6dd72a16440cde33dcf6bee48cc3f611b7ca
Julien Grillot <julien.grillot@gmail.com>
2013-03-21 16:49:23 +0100

    Update HAML (native markdown filter)

--- config/initializers/haml_markdown.rb
+++
@@ -1,8 +0,0 @@
-module Haml::Filters::Markdown
-  include Haml::Filters::Base
-  lazy_require "redcarpet"
-
-  def render(text)
-    Redcarpet::Markdown.new(Redcarpet::Render::HTML.new).render(text)
-  end
-end
