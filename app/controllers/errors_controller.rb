502b44c3ff7c14a7d63f7eb78d92725727302e32
Julien Grillot <julien.grillot@gmail.com>
2013-03-25 23:23:45 +0100

    Fix widget_mode (span12) for /asdf/404

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,11 @@
 class ErrorsController < ApplicationController
   def not_found
+    @widgets_mode = true
     render status: 404
   end
 
   def server_error
+    @widgets_mode = true
     render status: 500
   end
 end
