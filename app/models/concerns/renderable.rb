5e010544edb525225ba468ea29ec8bc1384c5ce9
Julien Grillot <julien.grillot@gmail.com>
2013-04-01 12:14:16 +0200

    Refactoring Renderable concern

--- app/models/concerns/renderable.rb
+++ app/models/concerns/renderable.rb
@@ -9,11 +9,14 @@ module Renderable
   private
 
   def render_content
+    self.rendered_content = render_tags(self.content)
+    self.rendered_content = render_markdown(self.rendered_content)
+    self.rendered_content = render_media(self.rendered_content)
+  end
+
+  def render_tags content
     @user_ids = Array.new
-    require 'redcarpet'
-    renderer = Redcarpet::Render::HTML.new filter_html: true, no_images: true, safe_links_only: true, with_toc_data: true
-    extensions = {space_after_headers: true, no_intra_emphasis: true, tables: true, fenced_code_blocks: true, autolink: false, strikethrough: true, superscript: true}
-    hashtagged = self.content.gsub(/(^|\s)@([[:alnum:]_-]+)/u) { |tag|
+    content.gsub(/(^|\s)@([[:alnum:]_-]+)/u) { |tag|
       if user = User.where(name: $2).first
         @user_ids << user.id
         "#{$1}[@#{$2}](#{Rails.application.routes.url_helpers.user_path(user)})"
@@ -23,9 +26,33 @@ module Renderable
     }.gsub(/(^|\s)#([[:alnum:]_-]+)/u) { |tag|
       "#{$1}[##{$2}](#{Rails.application.routes.url_helpers.messages_path(q: $2)})"
     }
-    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
-    self.rendered_content = redcarpet.render(hashtagged)
-    self.rendered_content = auto_html(self.rendered_content) {
+  end
+
+  def render_markdown content
+    require 'redcarpet'
+
+    renderer = Redcarpet::Render::HTML.new(
+      filter_html: true,
+      no_images: true,
+      safe_links_only: true,
+      with_toc_data: true
+    )
+
+    extensions = {
+      autolink: false,
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      space_after_headers: true,
+      strikethrough: true,
+      superscript: true,
+      tables: true
+    }
+
+    Redcarpet::Markdown.new(renderer, extensions).render(content)
+  end
+
+  def render_media content
+    auto_html(content) {
       image
       youtube
       dailymotion
